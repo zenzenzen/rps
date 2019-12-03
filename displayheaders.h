30bc027930d8dadfca23386277aed945a3a07645
johnsondeng <johnson.jy.deng@gmail.com>
2019-12-02 18:22:06 -0800

    moved macros into their own header file.

---
+++ displayheaders.h
@@ -0,0 +1,27 @@
+#define GREETING1 "\n|||||||||WELCOME TO RPS||||||||||\n 1 = ROCK ! 2 = PAPER ! 3 = SCISSORS !"
+#define GREETING2 "\nCHOOSE YOUR WEAPON, KNAVE: "
+#define RECEIVECHOICE "\n\nYOU HAVE CHOSEN...%s"
+#define SHOWPUTER "\nAND PUTER HAS CHOSEN...%s"
+#define BLAAAM1 "\n++++++++++++-------++++++++++++\n-----------+++++++++-----------\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+#define TIE "\nYOU SHALL LIVE, BUT NEITHER SHALL YOU WIN..."
+#define WIN "\n\nYOU HAVE WON. WIPE THE BLOOD OFF AND CELEBRATE WITH BOOZE AND BED\nOH GREAT CHAMPION...."
+#define LOSE "\nYOU HAVE DIED."
+
+#define SHAMEFULDEFEAT "\nYOU HAVE FAILED TO CHOOSE A WEAPON.\nYOU ARE KILLED WHERE YOU STAND!"
+#define EXITPROMPT "\n\nDo you want to continue or quit?\n[PRESS ANY OTHER KEY TO CONTINUE]\nType 'quit' to stop this program: "
+#define QUITTING "\n\nQuitting program. Have a nice day!"
+#define SCORES "\n\nSCORES:\tPlayer: %d\t\tPuter: %d"
+enum RPS{
+      NADA,
+      ROCK,
+      PAPER,
+      SCISSOR
+};
+#define rock "ROCK"
+#define paper "PAPER"
+#define scissor "SCISSORS"
+
+struct scores{
+  int computer;
+  int player;
+};
